5f4ffcd19e7ddcafa5e970bc11fb3b3bb8972ddb
Dan <danielmburu674@gmail.com>
2016-09-28 20:15:15 +0300

    Add update_at in table user; correct fixtures.yml; be able to create DB record using TestDummy: Pass the Login test

--- tests/functional/SighInCept.php
+++ tests/functional/SighInCept.php
@@ -5,7 +5,7 @@ $I = new FunctionalTester($scenario);
 $I-> am('a larabook member');
 $I->wantTo('Login to my larabook account');
 
-$I->haveAnAccount('email' => 'foo@example.com', 'password' => 'foo');
+$I->haveAnAccount(['email' => 'foo@example.com', 'password' => 'foo']);
 $I->amOnPage('/login');
 $I->fillField('email', 'foo@example.com');
 $I->fillField('password', 'foo');
