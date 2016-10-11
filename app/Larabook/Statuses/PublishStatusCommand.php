111cee8cdd2e0dc6a997a9d30152abafb705b757
Dan <danielmburu674@gmail.com>
2016-10-11 05:30:19 +0300

    Created Statuses Command: To create StatusesCommandHandler: 16-16

---
+++ app/Larabook/Statuses/PublishStatusCommand.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: daniel
+ * Date: 10/11/16
+ * Time: 5:19 AM
+ */
+
+namespace Larabook\Statuses;
+
+
+class PublishStatusCommand
+{
+
+}
---
+++ app/Larabook/Statuses/Status.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: daniel
+ * Date: 10/11/16
+ * Time: 4:52 AM
+ */
+
+namespace Larabook\Statuses;
+
+
+class Status
+{
+
+}
