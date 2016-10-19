76b80993729adab9b74c9d59626f4f15295c6996
Dan <danielmburu674@gmail.com>
2016-10-19 13:16:41 +0300

    Style

--- app/views/password/remind.blade.php
+++ app/views/password/remind.blade.php
@@ -1,24 +1,25 @@
 @extends('layouts.default')
+<div style="padding-top: 60px;">
+    @section('content')
+        <div  class="col-md-8 col-lg-offset-2">
+            <h1> Need to remember your password? </h1>
 
-@section('content')
-    <div style="padding-top: 60px;" class="col-md-8 col-lg-offset-2">
-        <h1> Need to remember your password? </h1>
 
+            {{ Form::open() }}
 
-        {{ Form::open() }}
+            <div class="form-group">
+                {{ Form::label('email', 'Email:') }}
+                {{ Form::text('email', null, ['class'=>'form-control']) }}
+            </div>
 
-        <div class="form-group">
-            {{ Form::label('email', 'Email:') }}
-            {{ Form::text('email', null, ['class'=>'form-control']) }}
+            <div class="form-group">
+                {{ Form::submit('Reset Password', ['class'=>'btn btn-primary form-control']) }}
+            </div>
+
+            {{Form::close()}}
+
         </div>
 
-        <div class="form-group">
-            {{ Form::submit('Reset Password', ['class'=>'btn btn-primary form-control']) }}
-        </div>
 
-        {{Form::close()}}
-
-    </div>
-
-
-@stop
+    @stop
+</div>
